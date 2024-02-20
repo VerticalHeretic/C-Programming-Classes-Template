3dcd2146f9f381a84d95bb0fa063c3699bf51461
PatrykPietruszewski <147393955+PatrykPietruszewski@users.noreply.github.com>
2024-02-20 12:35:04 +0100

    Update Zadanie_3.c

--- Zadanie_3.c
+++ Zadanie_3.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+void Licz_srednia(int array[]){
     
     int wynik[10];
     wynik[0]=array[0];
@@ -13,8 +12,15 @@ int main()
         wynik[i] = array[i]*array[i-1];
         
         printf("Wynik to: %d \n",wynik[i]);
-    }
+        }
+}
+
+
+int main()
+{
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     
+    Licz_srednia(array);
     
 	return 0;
 }
