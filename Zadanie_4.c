efd1a9754a7cc140b156a3b5524d78e38f3c128d
PatrykPietruszewski <147393955+PatrykPietruszewski@users.noreply.github.com>
2024-02-20 12:41:05 +0100

    Update Zadanie_4.c

--- Zadanie_4.c
+++ Zadanie_4.c
@@ -4,20 +4,17 @@
 int main()
 {
     for(int i = 1; i<=200; i++){
-        
-        switch( i ){
-            
-            case i%5==0:
-                printf("Foo");
-                break;
-            case i%7==0:
-                printf("Bar");
-                break;
-            case i%5 == 0 && i%7 == 0:
-                printf("FooBar");
-                break;
-            default:
-                printf("%d", i);
+        if(i%5 == 0 && i%7 == 0){
+            printf("FooBar \n");
+        }
+        else if(i%7 == 0){
+            printf("Bar \n");
+        }
+        else if(i%5 == 0 ){
+            printf("Foo \n");
+        }
+        else{
+            printf("%d \n", i);
         }
     }
 	return 0;
