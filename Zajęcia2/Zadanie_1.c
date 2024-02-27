acd37bf5117f9e1bd09852406b53e604e288f1c8
PatrykPietruszewski <147393955+PatrykPietruszewski@users.noreply.github.com>
2024-02-27 11:18:28 +0100

    Create Zadanie_1.c

---
+++ Zajęcia2/Zadanie_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a = 1;
+    int * pointerA = &a;
+    
+    //Zwiększaamy wartość po wskaźniku
+    (*pointerA)++;
+    
+    printf("Wartość a to %d\n", a);
+    printf("Wartość a po wskaźniku to %d\n", *pointerA);
+    
+    return 0;
+}
