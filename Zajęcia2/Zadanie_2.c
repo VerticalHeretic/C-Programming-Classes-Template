47af72628c7c99503a22a9fac5c963cc7a2db7e9
PatrykPietruszewski <147393955+PatrykPietruszewski@users.noreply.github.com>
2024-02-27 11:23:56 +0100

    Update Zadanie_2.c

--- Zajęcia2/Zadanie_2.c
+++ Zajęcia2/Zadanie_2.c
@@ -2,12 +2,13 @@
 
 void Modyfikuj(float *x){
     
-    
+    *x = (*x)*2;
 }
 
 int main()
 {
-    
-    
+    float a = 1.2;
+    Modyfikuj(&a);
+    printf("Nowa wartość a to %f", a);
     return 0;
 }
