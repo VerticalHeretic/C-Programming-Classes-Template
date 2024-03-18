e0ab3c30f926e7c5844f1373925217f5357150c9
Łukasz Stachnik <ls.warss98@gmail.com>
2024-03-18 14:53:30 +0100

    Sliming down the compile.sh, adding a little bit more comments

--- compile.sh
+++ compile.sh
@@ -3,14 +3,12 @@
 # Find all C files recursively in subdirectories
 find . -name '*.c' | while read file; do
     if [ -f "$file" ]; then
-        filename=$(basename "$file" .c)
-        output_path=$(dirname "$file")
+        filename=$(basename "$file" .c) # Extract filename without extension
+        output_path=$(dirname "$file") # Extract directory name
         clang -o "${output_path}/${filename}" "$file" -lm
-        clang -o "${output_path}/${filename}.exe" "$file" -lm
         if [ $? -eq 0 ]; then
             echo "Compilation successful for $file"
         else
-            echo "Error compiling $file"
             exit 1 # Exit the pipeline with a non-zero status code
         fi
     fi
